583ecbad4ff6d574933fbb87d0f036880fed78ff
Matheo <mflorez3@live.com>
2024-03-24 19:16:38 -0400

    Routing set-up

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Store } from './pages/Store';
 import { About } from './pages/About';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <NavBar />
+      <div className="container mx-auto">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,38 @@
+import { Link, NavLink } from 'react-router-dom';
+import { BsCartFill } from 'react-icons/bs';
+
+export const NavBar = () => {
+  return (
+    <nav className="shadow-md mb-3 bg-white sticky">
+      <div className="container mx-auto flex justify-between">
+        <ul>
+          <Link to={'/'}>Home</Link>
+          <NavLink
+            className={({ isActive }) =>
+              [isActive ? ' bg-blue-300 p-2 rounded-md' : 'p-2'].join(' ')
+            }
+            to={'/store'}
+          >
+            Store
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              [isActive ? ' bg-blue-300 p-2 rounded-md' : 'p-2'].join(' ')
+            }
+            to={'/about'}
+          >
+            About
+          </NavLink>
+        </ul>
+        <div className="mr-4 md:mr-0">
+          <button className="bg-transparent hover:bg-blue-500 p-3 border border-blue-500 rounded-full relative hover:border-transparent text-blue-500 hover:text-white">
+            <BsCartFill className="w-6 h-6" />
+            <div className="absolute -bottom-2 -right-2 z-40  bg-red-500 text-white rounded-full px-2 flex justify-center items-center">
+              1
+            </div>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
