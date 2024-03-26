9bde19e91bca56dd4f434cf258ff4c08d9ef74e9
Matheo <mflorez3@live.com>
2024-03-26 00:41:01 -0400

    added images and created template json data for card display

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,17 @@
+import products from '../data/items.json';
+import Products from '../components/Products';
+
 export const Store = () => {
-  return <h1>Store</h1>;
+  return (
+    <>
+      <h1>Store</h1>
+      <div className="sm:columns-1 md:columns-2 lg:columns-2">
+        {products.map((item) => (
+          <div key={item.id}>
+            <Products {...item} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
