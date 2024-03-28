d46e537b4bdea8073289cdefe4ccbcd4c23ec036
Matheo <mflorez3@live.com>
2024-03-28 00:54:24 -0400

    conditional rendering for item quantity within item cards

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -8,6 +8,7 @@ type ProductProps = {
 };
 
 const Products = ({ id, name, price, imgUrl }: ProductProps) => {
+  let quantity = 1;
   return (
     <div
       key={id}
@@ -24,24 +25,44 @@ const Products = ({ id, name, price, imgUrl }: ProductProps) => {
             {currencyFormat(price)}
           </p>
         </div>
-        <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Add to Cart
-          <svg
-            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </button>
+        {quantity === 0 ? (
+          <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Add to Cart
+            <svg
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </button>
+        ) : (
+          <div className="flex-col items-center">
+            <div className="flex justify-evenly">
+              <button className="text-xl font-semibold w-10 h-10 hover:bg-blue-500 bg-blue-600 text-white rounded-full active:bg-blue-700 transiton transform duration-200">
+                -
+              </button>
+              <div>
+                <span className="text-2xl font-semibold">{quantity}</span> in
+                cart
+              </div>
+              <button className=" text-xl font-semibold w-10 h-10 hover:bg-blue-500 bg-blue-600 text-white rounded-full active:bg-blue-700 transiton transform duration-200">
+                +
+              </button>
+            </div>
+            <button className="bg-red-700 active:bg-red-800 font-semibold hover:bg-red-600 text-white px-2 py-3 rounded-md transiton transform duration-200">
+              Remove
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
